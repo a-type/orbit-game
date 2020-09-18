31b3fc7759dcc3e0a0da7c7de5c1c784c2761c2b
Grant Forrest <grant@grants-mbp.lan>
2020-09-18 11:41:20 -0400

    feat: basic lit scene

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,7 +1,6 @@
-import { OrbitControls, PerspectiveCamera } from 'drei';
+import { OrbitControls, PerspectiveCamera, Plane, RoundedBox } from 'drei';
 import React, { FC } from 'react';
 import { Canvas } from 'react-three-fiber';
-import Cube from './meshes/Cube';
 import styles from './Scene.module.css';
 
 export interface SceneProps {}
@@ -9,7 +8,21 @@ export interface SceneProps {}
 const Scene: FC<SceneProps> = ({}) => {
   return (
     <Canvas className={styles.scene}>
-      <Cube position={[0, 0, 0]} />
+      <RoundedBox
+        position={[0, 0, 0]}
+        args={[1, 1, 1]}
+        radius={0.05}
+        smoothness={4}
+      >
+        <meshPhongMaterial attach="material" color="green" />
+      </RoundedBox>
+      <Plane
+        position={[0, -5, 0]}
+        args={[100, 100]}
+        rotation={[Math.PI * 1.5, 0, 0]}
+      >
+        <meshPhongMaterial attach="material" color="white" />
+      </Plane>
       <pointLight position={[10, 10, 10]} />
       <PerspectiveCamera makeDefault position={[0, 0, 10]} />
       <OrbitControls />
